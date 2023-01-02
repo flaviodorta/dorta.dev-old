ad4d7b565ff3ae96eb1da8b52890eb45eb4ddb51
Flavio Dorta <dorta.dev@gmail.com>
2023-01-02 12:16:27 -0300

    feat: add jet brains font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,10 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:wght@300;400;500;600;700;800;900&family=Share+Tech+Mono&display=swap'
           rel='stylesheet'
         />
+        <link
+          href='https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <body>
         <Main />
